a2895ffe2924fce68272035a917d2f845eceec0a
Ezpert <109883612+Ezpert@users.noreply.github.com>
2023-10-07 22:23:45 -0500

    Added Cat Function

--- responses.py
+++ responses.py
@@ -4,17 +4,18 @@ import random
 def handle_response(message) -> str:
     p_message = message.lower()
 
-
-    if p_message == 'roll':
+    if p_message.lower() == 'roll':
         return str(random.randint(1, 6))
+    if p_message.lower() == "cat":
+        return 'cat.jpg'
 
-    if p_message == 'hello':
+    if p_message.lower() == 'hello':
         return 'Hey There!'
 
-    if p_message == 'help':
+    if p_message.lower() == 'help':
         return 'This is help message that you can modify.'
 
-    if p_message == 'robert':
+    if p_message.lower() == 'robert':
         return 'Hello, my name is RobBot Thibodeaux I dont get girls and love "The Beatles"'
 
 
